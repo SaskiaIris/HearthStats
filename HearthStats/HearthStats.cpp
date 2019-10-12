569333a24d07b08435a52a5b5b082eaa257c0e52
IrisSaskia <iris.oostra@hva.nl>
2019-10-12 22:28:42 +0200

    Alsjeblieeft
    Haaaaaiiiiiii

--- HearthStats/HearthStats.cpp
+++ HearthStats/HearthStats.cpp
@@ -14,46 +14,63 @@ using namespace std::this_thread;
 using namespace std::chrono_literals;
 using std::chrono::system_clock;
 
-typedef enum StateIDs{
-	StartGame,
+typedef enum RobberStateIDs{
+	StartGameRobber,
 	RobbingBank,
 	HavingGoodTime,
 	LayingLow,
 	Fleeing,
 	BeingInJail,
 	BeingTipsy
-} State;
+} RobberState;
 
-//Max value = 100 for wealth and strength, 10 for distanceCop
+typedef enum CopStateIDs {
+	StartGameCop,
+	OnStakeOut,
+	Chasing,
+	OffDuty
+} CopState;
+
+int fps = 60;
+
+//Max value = 100 for wealth and strength, 10 for distanceCop, values for the thief
 int wealth;
 int distanceCop;
 int strength;
 
-int fps = 60;
+//The dutyTime has a max value of 10
+int dutyTime;
 
-State currentState = StartGame;
+RobberState currentRobberState = StartGameRobber;
+CopState currentCopState = StartGameCop;
 
-string actionInput;
-string actionPerformingText;
+string actionRobberPerformingText;
+string actionCopPerformingText;
 
 void playGame();
 
-void checkMax();
-void varChangeByState();
-State stateManager(State beforeState, int currentWealth, int currentStrength, int currentDistanceCop);
-string stateMessage();
+void checkMax(); //Check whether the variables haven't exceeded their maximum and minimum values
+
+void varChangeByStateRobber();
+RobberState stateRobberManager(RobberState beforeState, int currentWealth, int currentStrength, int currentDistanceCop);
+string stateRobberMessage();
+
+
+void varChangeByStateCop();
+CopState stateCopManager(CopState beforeState, int currentDutyTime);
+string stateCopMessage();
 
 int main() {
 	srand(time(NULL));
 
 	cout << "Press enter to begin." << endl;
 	if (cin.get() == '\n') {
-		varChangeByState();
-		checkMax(); //Check whether the variables haven't exceeded their maximum and minimum values
+		varChangeByStateRobber();
+		checkMax();
 
-		currentState = stateManager(currentState, wealth, strength, distanceCop);
-		cout << actionPerformingText << endl;
-		cout << stateMessage() << endl << endl << endl;
+		currentRobberState = stateRobberManager(currentRobberState, wealth, strength, distanceCop);
+		cout << actionRobberPerformingText << endl;
+		cout << stateRobberMessage() << endl << endl << endl;
 		playGame();
 	}
 
@@ -62,24 +79,24 @@ int main() {
 
 void playGame() {
 	for (int i = 1; i < fps; i++) {
-		varChangeByState();
+		varChangeByStateRobber();
 		checkMax(); //Check whether the variables haven't exceeded their maximum and minimum values
 	}
 
 	sleep_until(system_clock::now() + 2.5s);
 
-	currentState = stateManager(currentState, wealth, strength, distanceCop);
+	currentRobberState = stateRobberManager(currentRobberState, wealth, strength, distanceCop);
 	cout << "Wealth: " << wealth << endl << "Strength: " << strength << endl << "Distance to cop: " << distanceCop << endl << endl;
-	cout << actionPerformingText << endl << endl;
-	cout << stateMessage() << endl << endl << endl << endl;
+	cout << actionRobberPerformingText << endl << endl;
+	cout << stateRobberMessage() << endl << endl << endl << endl;
 
-	/*if (cin.get() == 'exit') {
+	if (cin.get() == 'exit') {
 		return;
 	}
-	else {*/
+	else {
 		//cout << "What's next? (type exit to quit the game)" << endl;
 		playGame();
-	//}
+	}
 }
 
 void checkMax() {
@@ -100,11 +117,18 @@ void checkMax() {
 	} else if(wealth < 0) {
 		wealth = 0;
 	}
+
+	if (dutyTime > 10) {
+		dutyTime = 10;
+	}
+	else if(dutyTime < 0) {
+		dutyTime = 0;
+	}
 }
 
-void varChangeByState(){
-	switch(currentState) {
-		case StartGame:
+void varChangeByStateRobber(){
+	switch(currentRobberState) {
+		case StartGameRobber:
 			wealth = 0;
 			strength = 100;
 			distanceCop = rand() % 9;
@@ -153,138 +177,138 @@ void varChangeByState(){
 	}
 }
 
-State stateManager(State beforeState, int currentWealth, int currentStrength, int currentDistanceCop) {
-	State outputState;
+RobberState stateRobberManager(RobberState beforeState, int currentWealth, int currentStrength, int currentDistanceCop) {
+	RobberState outputState;
 
 	switch (beforeState) {
-		case StartGame:
+		case StartGameRobber:
 			if (currentWealth == 0 && currentStrength == 100) {
 				outputState = RobbingBank;
-				actionPerformingText = "\"Let\'s start this\"";
+				actionRobberPerformingText = "\"Let\'s start this\"";
 			}
 			else {
 				outputState = beforeState;
-				actionPerformingText = "\"I\'m so confused\" + ERROR CHECK STARTGAME";
+				actionRobberPerformingText = "\"I\'m so confused\" + ERROR CHECK STARTGAME";
 			}
 			break;
 		case RobbingBank:
 			if (currentWealth >= 60 && currentDistanceCop > 5) {
 				//GetRich
 				outputState = HavingGoodTime;
-				actionPerformingText = "\"I\'m rich enough to have a good time\"";
+				actionRobberPerformingText = "\"I\'m rich enough to have a good time\"";
 			}
 			else if (currentDistanceCop > 0 && currentDistanceCop <= 5) {
 				//SpotCop
 				outputState = Fleeing;
-				actionPerformingText = "\"I see a cop, so I have to start running\"";
+				actionRobberPerformingText = "\"I see a cop, so I have to start running\"";
 			}
 			else if (currentDistanceCop == 0) {
 				//GetCaught
 				outputState = BeingInJail;
-				actionPerformingText = "\"Oh no, the cops are bringing me to my new home!\"";
+				actionRobberPerformingText = "\"Oh no, the cops are bringing me to my new home!\"";
 			}
 			else {
 				outputState = beforeState;
-				actionPerformingText = "\"Let\'s try again\" + ERROR CHECK ROBBINGBANK";
+				actionRobberPerformingText = "\"Let\'s try again\" + ERROR CHECK ROBBINGBANK";
 			}
 			break;
 		case HavingGoodTime:
 			if (currentWealth >= 40 && currentWealth <= 70 && currentStrength < 40) {
 				//GetTired
 				outputState = LayingLow;
-				actionPerformingText = "\"I\'m getting very tired, so I better lay low for a while\"";
+				actionRobberPerformingText = "\"I\'m getting very tired, so I better lay low for a while\"";
 			}
 			else if (currentDistanceCop == 0) {
 				//SpotCop
 				outputState = Fleeing;
-				actionPerformingText = "\"I see a cop, so I have to start running\"";
+				actionRobberPerformingText = "\"I see a cop, so I have to start running\"";
 			}
 			else if (currentStrength < 40) {
 				//GoToBar
 				outputState = BeingTipsy;
-				actionPerformingText = "\"All drinks on me!\"";
+				actionRobberPerformingText = "\"All drinks on me!\"";
 			}
 			else {
 				outputState = beforeState;
-				actionPerformingText = "\"Let\'s try again\" + ERRO CHECK HAVINGGOODTIME";
+				actionRobberPerformingText = "\"Let\'s try again\" + ERRO CHECK HAVINGGOODTIME";
 			}
 			break;
 		case LayingLow:
 			if (currentStrength >= 65 && currentDistanceCop >= 7) {
 				//FeelSafe
 				outputState = RobbingBank;
-				actionPerformingText = "\"Let\'s get back to what I was doing\"";
+				actionRobberPerformingText = "\"Let\'s get back to what I was doing\"";
 			}
 			else if (currentStrength < 65 && currentWealth >= 40 && currentWealth <= 70) {
 				//GetBored
 				outputState = BeingTipsy;
-				actionPerformingText = "\"A bar is a good safe spot, right? Right??\"";
+				actionRobberPerformingText = "\"A bar is a good safe spot, right? Right??\"";
 			}
 			else {
 				outputState = beforeState;
-				actionPerformingText = "\"Let\'s try again\" + ERROR CHECK LAYINGLOW";
+				actionRobberPerformingText = "\"Let\'s try again\" + ERROR CHECK LAYINGLOW";
 			}
 			break;
 		case Fleeing:
 			if (currentDistanceCop < 7 && currentDistanceCop > 0) {
 				//GetTired
 				outputState = LayingLow;
-				actionPerformingText = "\"I\'m getting very tired, so I better lay low for a while\"";
+				actionRobberPerformingText = "\"I\'m getting very tired, so I better lay low for a while\"";
 			}
 			else if (currentDistanceCop >= 7) {
 				//FeelSafe
 				outputState = RobbingBank;
-				actionPerformingText = "\"Let\'s get back to what I was doing\"";
+				actionRobberPerformingText = "\"Let\'s get back to what I was doing\"";
 			}
 			else if (currentDistanceCop == 0 && currentStrength < 50) {
 				//GetCaught
 				outputState = BeingInJail;
-				actionPerformingText = "\"Oh no, the cops are bringing me to my new home!\"";
+				actionRobberPerformingText = "\"Oh no, the cops are bringing me to my new home!\"";
 			}
 			else {
 				outputState = beforeState;
-				actionPerformingText = "\"Let\'s try again\" + ERROR CHECK FLEEING";
+				actionRobberPerformingText = "\"Let\'s try again\" + ERROR CHECK FLEEING";
 			}
 			break;
 		case BeingInJail:
 			if (currentStrength >= 70) {
 				//Escape
 				outputState = Fleeing;
-				actionPerformingText = "\"RUUUNNN!! RUUUUUUUUNNNN!!!\"";
+				actionRobberPerformingText = "\"RUUUNNN!! RUUUUUUUUNNNN!!!\"";
 			}
 			else {
 				outputState = beforeState;
-				actionPerformingText = "\"Let\'s try again\" + ERROR CHECK BEINGINJAIL";
+				actionRobberPerformingText = "\"Let\'s try again\" + ERROR CHECK BEINGINJAIL";
 			}
 			break;
 		case BeingTipsy:
 			if (currentStrength < 40) {
 				//Tell too much
 				outputState = BeingInJail;
-				actionPerformingText = "\"No! Wait! Those were just stories!\"";
+				actionRobberPerformingText = "\"No! Wait! Those were just stories!\"";
 			}
 			else if (currentStrength >= 40 && currentWealth < 40) {
 				//Get bored
 				outputState = RobbingBank;
-				actionPerformingText = "\"You know what would be funny..? If we go \'borrow\' something from the bank…hehe\"";
+				actionRobberPerformingText = "\"You know what would be funny..? If we go \'borrow\' something from the bank…hehe\"";
 			}
 			else {
 				outputState = beforeState;
-				actionPerformingText = "\"Let\'s try again\"";
+				actionRobberPerformingText = "\"Let\'s try again\"";
 			}
 			break;
 		default:
-			outputState = StartGame;
-			actionPerformingText = "\"Let\'s start over\" + ERROR CHECK DEFAULTSTATE";
+			outputState = StartGameRobber;
+			actionRobberPerformingText = "\"Let\'s start over\" + ERROR CHECK DEFAULTSTATE";
 	}
 
 	return outputState;
 }
 
-string stateMessage() {
+string stateRobberMessage() {
 	string outputStateMessage;
 
-	switch (currentState) {
+	switch (currentRobberState) {
 		case RobbingBank:
 			outputStateMessage = "\"'I\'m robbing banks and getting loads of money! Pew pew!\"";
 			break;
@@ -304,7 +328,107 @@ string stateMessage() {
 			outputStateMessage = "\"Glug..glug.... hmmmm..... beeeer\"";
 			break;
 		default:
-			outputStateMessage = "\"I am so confused\" + ERROR CHECK DEFAULTMESSAGE";
+			outputStateMessage = "\"I am so confused\" + ERROR CHECK DEFAULTMESSAGEROBBER";
+	}
+
+	return outputStateMessage;
+}
+
+void varChangeByStateCop() {
+	switch (currentCopState) {
+		case StartGameCop:
+			dutyTime = 0;
+
+			break;
+		case OffDuty:
+			dutyTime -= rand() % 2;
+
+			break;
+		case OnStakeOut:
+			dutyTime += rand() % 2;
+
+			break;
+		case Chasing:
+			dutyTime += rand() % 2;
+			distanceCop--;
+
+			break;
+		default:
+			dutyTime = 0;
+	}
+}
+
+CopState stateCopManager(CopState beforeState, int currentDutyTime) {
+	CopState outputState;
+
+	switch (beforeState) {
+	case StartGameCop:
+		if (dutyTime < 1) {
+			outputState = OnStakeOut;
+			actionCopPerformingText = "\"Let\'s start this\"";
+		}
+		else {
+			outputState = beforeState;
+			actionCopPerformingText = "\"I\'m so confused\" + ERROR CHECK STARTGAME";
+		}
+		break;
+	case OnStakeOut:
+		if (distanceCop < 5) {
+			outputState = Chasing;
+			actionCopPerformingText = "\"I\'m rich enough to have a good time\"";
+		}
+		else {
+			outputState = beforeState;
+			actionCopPerformingText = "\"Let\'s try again\" + ERROR CHECK ROBBINGBANK";
+		}
+		break;
+	case Chasing:
+		if (dutyTime > 9 && currentRobberState != BeingInJail) {
+			outputState = OffDuty;
+			actionCopPerformingText = "\"I\'m getting very tired, so I better lay low for a while\"";
+		}
+		else if (dutyTime > 9 && currentRobberState == BeingInJail) {
+			outputState = OffDuty;
+			actionCopPerformingText = "\"I\'m getting very tired, so I better lay low for a while\"";
+		}
+		else {
+			outputState = beforeState;
+			actionCopPerformingText = "\"Let\'s try again\" + ERRO CHECK HAVINGGOODTIME";
+		}
+		break;
+	case OffDuty:
+		if (dutyTime = 0) {
+			outputState = OnStakeOut;
+			actionCopPerformingText = "\"I\'m getting very tired, so I better lay low for a while\"";
+		}
+		else {
+			outputState = beforeState;
+			actionCopPerformingText = "\"Let\'s try again\" + ERRO CHECK HAVINGGOODTIME";
+		}
+		break;
+	default:
+		outputState = StartGameCop;
+		actionCopPerformingText = "\"Let\'s start over\" + ERROR CHECK DEFAULTSTATE";
+	}
+
+	return outputState;
+}
+
+string stateCopMessage() {
+	string outputStateMessage;
+
+	switch (currentCopState) {
+		case OffDuty:
+			outputStateMessage = "\"'I\'m robbing banks and getting loads of money! Pew pew!\"";
+			break;
+		case OnStakeOut:
+			outputStateMessage = "\"I\'m having a good time spending my money\"";
+			break;
+		case Chasing:
+			outputStateMessage = "\"Hold it right there buddy\"";
+			break;
+		default:
+			outputStateMessage = "\"I need a donut\" + ERROR CHECK DEFAULTMESSAGECOP";
 	}
 
 	return outputStateMessage;
